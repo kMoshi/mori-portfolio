6bf42f9a7e6f825e1a818d9c87bd792d63688d6b
kMoshi <ourohbird@gmail.com>
2024-06-07 23:47:10 +0900

    test

--- .netlify/functions-internal/ssr/chunks/generic_DcGvv7w-.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/generic_BQb_71gv.mjs');
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/index_ByJY4KAH.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_kRBZyAC7.mjs');
-
-export { page };
